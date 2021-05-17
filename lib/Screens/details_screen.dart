5c1b2e22ae20c8260e9324e051367ff27e8af700
Argania's PC <Sadok.laouissi@esprit.tn>
2021-05-17 17:10:38 +0100

    Details Screen Update

--- lib/Screens/details_screen.dart
+++ lib/Screens/details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -27,21 +28,19 @@ class _DetailsScreenState extends State<DetailsScreen> {
       menu: buildMenu(context),
       type: SideMenuType.slideNRotate,
       child: Scaffold(
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20.0,
-              vertical: 40.0,
-            ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 0.0,
+            vertical: 40.0,
+          ),
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Stack(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
+                      padding: const EdgeInsets.only(top: 8.0, left:20),
                       child: GestureDetector(
                           onTap: () => Navigator.pop(context),
                           child: Icon(
@@ -63,40 +62,47 @@ class _DetailsScreenState extends State<DetailsScreen> {
                     buildDrawerButton(),
                   ],
                 ),
-                Divider(),
+                Padding(
+                  padding: const EdgeInsets.only(left:20,right: 20),
+                  child: Divider(),
+                ),
                 SizedBox(
                   height: height * 2 / 100,
                 ),
-                Text(
-                  "GUERLAIN SHALIMAR",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0,
-                      color: AppTheme.primaryColor),
-                ),
-                Text(
-                  "Eau de parfum",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20.0,
-                      color: AppTheme.primaryColor),
+                Padding(
+                  padding: const EdgeInsets.only(left:20,right: 20),
+                  child: Text(
+                    "GUERLAIN SHALIMAR",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0,
+                        color: AppTheme.primaryColor),
+                  ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
+                  padding: const EdgeInsets.only(left:20,right: 20),
+                  child: Text(
+                    "Eau de parfum",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0,
+                        color: AppTheme.primaryColor),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0,left:20,right: 20),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
                         decoration: BoxDecoration(
                           color: AppTheme.primaryAccentColor,
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(20)),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
                         ),
                         child: Padding(
-                          padding:
-                          const EdgeInsets.only(left: 8.0, right: 8.0),
+                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
                           child: Text(
                             "138.0 TND",
                             textAlign: TextAlign.left,
@@ -107,7 +113,23 @@ class _DetailsScreenState extends State<DetailsScreen> {
                           ),
                         ),
                       ),
-
+                      Container(
+                        decoration: BoxDecoration(
+                          color: AppTheme.primaryColor,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                          child: Text(
+                            "100 mL",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                                color: AppTheme.whiteColor),
+                          ),
+                        ),
+                      ),
                       Row(
                         children: [
                           Container(
@@ -119,8 +141,8 @@ class _DetailsScreenState extends State<DetailsScreen> {
                             child: Row(
                               children: [
                                 Padding(
-                                  padding:
-                                      const EdgeInsets.only(left: 8.0, right: 3.0),
+                                  padding: const EdgeInsets.only(
+                                      left: 8.0, right: 3.0),
                                   child: Text(
                                     "14/20",
                                     textAlign: TextAlign.left,
@@ -133,9 +155,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                                 Padding(
                                   padding: const EdgeInsets.only(right: 8.0),
                                   child: GestureDetector(
-                                      onTap: ()=> {
-
-                                      },
+                                      onTap: () => {},
                                       child: FaIcon(
                                         FontAwesomeIcons.infoCircle,
                                         color: AppTheme.whiteColor,
@@ -145,12 +165,88 @@ class _DetailsScreenState extends State<DetailsScreen> {
                               ],
                             ),
                           ),
-
                         ],
                       )
                     ],
                   ),
                 ),
+
+                SizedBox(
+                  height: height * 3 / 100,
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/images/prod_image_left.png",width: width*50/100,),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: AutoSizeText(
+                        'Inspiré par l\'histoire d\'un amour fou entre un empereur et une princesse indienne, Shalimar, qui signifie en sanscrit "Temple de l\'amour", symbolise à jamais l\'essence du désir, la promesse d\'un amour éternel.',
+                        maxLines: 20,
+                        overflow: TextOverflow.fade,
+                        softWrap: true,
+                        style: TextStyle(
+
+                            fontWeight: FontWeight.normal,
+                            color: AppTheme.primaryColor),
+                      ),
+                    ),
+
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Plus d\'informations",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15.0,
+                          color: AppTheme.primaryColor),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset("assets/images/left_info_btn.png"),
+
+
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left:20,right: 20),
+                  child: Text(
+                    "Shalimar est le premier oriental de l'histoire de la parfumerie.",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15.0,
+                        color: AppTheme.primaryColor),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left:20,right: 20),
+                  child: Text(
+                    "Produits Similaires",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        color: AppTheme.primaryColor),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left:20,right: 20),
+                  child: _buildSugguestions(),
+                )
               ],
             ),
           ),
@@ -159,20 +255,76 @@ class _DetailsScreenState extends State<DetailsScreen> {
     );
   }
 
+  Widget _buildSugguestions() {
+    return DefaultTabController(
+        length: 3, // length of tabs
+        initialIndex: 0,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: TabBar(
+                  isScrollable: true,
+                  unselectedLabelStyle:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                  labelColor: AppTheme.primaryColor,
+                  unselectedLabelColor: AppTheme.greyColor,
+                  indicatorColor: Colors.transparent,
+                  labelPadding: EdgeInsets.only(left: 15),
+                  labelStyle:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  tabs: [
+                    Tab(text: 'Populaire'),
+                    Tab(text: 'Nouveau'),
+                    Tab(text: 'Récommandé'),
+                  ],
+                ),
+              ),
+              Container(
+                  height: 200, //height of TabBarView
+                  color: AppTheme.darkBlueColor,
+                  child: TabBarView(children: <Widget>[
+                    Container(
+                      child: Center(
+                        child: Text('Display Tab 1',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: Text('Display Tab 2',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: Text('Display Tab 3',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                  ]))
+            ]));
+  }
   buildDrawerButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        MaterialButton(
-          onPressed: () {},
-          height: 50.0,
-          minWidth: 50.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-          ),
-          child: FaIcon(FontAwesomeIcons.heart),
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(right:20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          MaterialButton(
+            onPressed: () {},
+            height: 50.0,
+            minWidth: 50.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: FaIcon(FontAwesomeIcons.heart),
+          )
+        ],
+      ),
     );
   }
 }
